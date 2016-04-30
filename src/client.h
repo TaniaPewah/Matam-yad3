2018eb7158fa5f2e4158881b2f5ac5423b440d56
alondBind <binder@binder-pc>
2016-04-30 13:21:00 +0300

    add client

---
+++ src/client.h
@@ -0,0 +1,20 @@
+/*
+ * Client.h
+ *
+ *  Created on: 30 באפר׳ 2016
+ *      Author: binder
+ */
+
+#ifndef SRC_CLIENT_H_
+#define SRC_CLIENT_H_
+
+typedef struct ListItem_t *Client;
+
+struct Cliet_t {
+	char* Email;
+	char* Name;
+	int Total_Mony_Paid;
+};
+
+
+#endif /* SRC_CLIENT_H_ */
