1256a150463fa3e92580ab07c26eb665796529a7
alondBind <binder@binder-pc>
2016-05-01 09:36:08 +0300

    s

---
+++ src/clientsManager_test.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdbool.h>
+#include "test_utilities.h"
+#include "clientManager.h"
+#include "client.h"
+
+int RunClientManagerTest();
+static bool testClientCreate();
+static bool testClientGetersAndSetters();
+
+//int main() {
+int RunClientManagerTest() {
+	RUN_TEST(testClientsManagerCreate);
+	return 0;
+}
+
+/**
+ * Test create & destroy method
+ */
+static bool testClientsManagerCreate() {
+	ClientsManager manager = clientsManagerCreate();
+	ASSERT_TEST(manager != NULL);
+	clientsManagerDestroy(manager);
+	return true;
+}
+
+/**
+ * Test create method
+ */
+static bool testClientsManagerAdd() {
+	char* good_mail = "baba@ganosh";
+	Client client;
+	clientCreate(good_mail,5, 5, 5, &client);
+	ASSERT_TEST(client != NULL);
+
+
+
+	clientDestroy(client);
+	return true;
+}
+
+
