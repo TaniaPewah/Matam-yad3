f7b707dfcf4a491613f59af93e3a981743565b16
alondBind <binder@binder-pc>
2016-05-04 00:47:48 +0300

    added test :)

--- src/client_test.c
+++ src/client_test.c
@@ -5,65 +5,101 @@
 #include <stdbool.h>
 #include "test_utilities.h"
 #include "client.h"
-//
-//int RunClientTest();
-//static bool testClientCreate();
-//static bool testClientGetersAndSetters();
-//
-////int main() {
-//int RunClientTest() {
-//	RUN_TEST(testClientCreate);
-//	RUN_TEST(testClientGetersAndSetters);
-//	return 0;
-//}
-//
-///**
-// * Test create method
-// */
-//static bool testClientCreate() {
-//	char* bad_mail = "babaganosh";
-//	char* good_mail = "baba@ganosh";
-//	Client client;
-//	ASSERT_TEST(clientCreate(NULL,5,5,5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(bad_mail,5,5,5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,-5, 5, 5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,5, -5, 5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,5, 5, -5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,0, 5, 5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,5, 0, 5, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,5, 5, 0, &client)
-//			== CLIENT_INVALID_PARAMETERS);
-//	ASSERT_TEST(clientCreate(good_mail,5, 5, 5, &client)
-//			== CLIENT_SUCCESS);
-//	ASSERT_TEST(client != NULL);
-//	clientDestroy(client);
-//	return true;
-//}
-//
-//static bool testClientGetersAndSetters() {
-//	char* good_mail = "baba@ganosh";
-//	Client client;
-//	clientCreate(good_mail, 5, 6, 7, &client);
-//	ASSERT_TEST(clientGetMail(NULL) == NULL);
-//	ASSERT_TEST(clientGetMinArea(NULL) == NO_CLIENT_VAL);
-//	ASSERT_TEST(clientGetMinRooms(NULL) == NO_CLIENT_VAL);
-//	ASSERT_TEST(clientGetMaxPrice(NULL) == NO_CLIENT_VAL);
-//	ASSERT_TEST(clientGetTotalPayments(NULL) == NO_CLIENT_VAL);
-//	ASSERT_TEST(strcmp(clientGetMail(client), good_mail) == 0);
-//	ASSERT_TEST(clientGetMinArea(client) == 5);
-//	ASSERT_TEST(clientGetMinRooms(client) == 6);
-//	ASSERT_TEST(clientGetMaxPrice(client) == 7);
-//	ASSERT_TEST(clientGetTotalPayments(client) == 0);
-//	clientAddPayment(client, 50);
-//	ASSERT_TEST(clientGetTotalPayments(client) == 50);
-//	clientDestroy(client);
-//	return true;
-//}
-//
+#include "email.h"
+
+int RunClientTest();
+static bool testClientCreate();
+static bool testClientGetersAndSetters();
+static bool testClientCopy();
+
+//int main() {
+int RunClientTest() {
+	RUN_TEST(testClientCreate);
+	RUN_TEST(testClientGetersAndSetters);
+	RUN_TEST(testClientCopy);
+	return 0;
+}
+
+/**
+ * Test create method
+ */
+static bool testClientCreate() {
+	char* good_mail = "baba@ganosh";
+	Email mail;
+	emailCreate(good_mail, &mail);
+	Client client;
+	ASSERT_TEST(clientCreate(NULL,5,5,5, &client)
+			== CLIENT_NULL_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,5, 5, 5, NULL)
+			== CLIENT_NULL_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,-5, 5, 5, &client)
+			== CLIENT_INVALID_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,5, -5, 5, &client)
+			== CLIENT_INVALID_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,5, 5, -5, &client)
+			== CLIENT_INVALID_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,0, 5, 5, &client)
+			== CLIENT_INVALID_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,5, 0, 5, &client)
+			== CLIENT_INVALID_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,5, 5, 0, &client)
+			== CLIENT_INVALID_PARAMETERS);
+	ASSERT_TEST(clientCreate(mail,5, 5, 5, &client)
+			== CLIENT_SUCCESS);
+	ASSERT_TEST(client != NULL);
+	clientDestroy(client);
+	emailDestroy(mail);
+	return true;
+}
+
+/**
+ * Test geters and seters method
+ */
+static bool testClientGetersAndSetters() {
+	char* mail_adress = "baba@ganosh";
+	Email good_mail;
+	emailCreate(mail_adress, &good_mail);
+	Client client;
+	clientCreate(good_mail, 5, 6, 7, &client);
+	ASSERT_TEST(clientGetMail(NULL) == NULL);
+	ASSERT_TEST(clientGetMinArea(NULL) == NO_CLIENT_VAL);
+	ASSERT_TEST(clientGetMinRooms(NULL) == NO_CLIENT_VAL);
+	ASSERT_TEST(clientGetMaxPrice(NULL) == NO_CLIENT_VAL);
+	ASSERT_TEST(clientGetTotalPayments(NULL) == NO_CLIENT_VAL);
+	ASSERT_TEST(emailAreEqual(clientGetMail(client), good_mail));
+	ASSERT_TEST(clientGetMinArea(client) == 5);
+	ASSERT_TEST(clientGetMinRooms(client) == 6);
+	ASSERT_TEST(clientGetMaxPrice(client) == 7);
+	ASSERT_TEST(clientGetTotalPayments(client) == 0);
+	clientAddPayment(client, 50);
+	ASSERT_TEST(clientGetTotalPayments(client) == 50);
+	clientDestroy(client);
+	emailDestroy(good_mail);
+	return true;
+}
+
+/**
+ * Test copy method
+ */
+static bool testClientCopy() {
+	char* good_mail = "baba@ganosh";
+	Client client, copy = NULL;
+	Email mail;
+	emailCreate(good_mail, &mail);
+	clientCreate(mail,5, 5, 5, &client);
+	clientAddPayment(client, 50);
+	ASSERT_TEST(clientCopy(NULL, NULL) == CLIENT_NULL_PARAMETERS);
+	ASSERT_TEST(clientCopy(client, NULL) == CLIENT_NULL_PARAMETERS);
+	ASSERT_TEST(clientCopy(NULL, &copy) == CLIENT_NULL_PARAMETERS);
+	ASSERT_TEST(clientCopy(client, &copy) == CLIENT_SUCCESS);
+	ASSERT_TEST(emailAreEqual(clientGetMail(client), clientGetMail(copy)));
+	ASSERT_TEST(clientGetMinArea(client) == clientGetMinArea(copy));
+	ASSERT_TEST(clientGetMinRooms(client) == clientGetMinArea(copy));
+	ASSERT_TEST(clientGetMaxPrice(client) == clientGetMinArea(copy));
+	ASSERT_TEST(
+		clientGetTotalPayments(client) == clientGetTotalPayments(copy));
+	clientDestroy(copy);
+	clientDestroy(client);
+	emailDestroy(mail);
+	return true;
+}
---
+++ src/email_test.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "test_utilities.h"
+#include "email.h"
+
+int RunEmailTest();
+static bool testEmailCreate();
+static bool testEmailCopy();
+
+//int main() {
+int RunEmailTest() {
+	RUN_TEST(testEmailCreate);
+	RUN_TEST(testEmailCopy);
+	return 0;
+}
+
+/**
+ * Test create method
+ */
+static bool testEmailCreate() {
+	char* good_mail = "baba@ganosh";
+	char* bad_mail_1  = "baba+ganosh";
+	char* bad_mail_2  = "";
+	Email mail;
+	ASSERT_TEST(emailCreate(NULL, NULL) == EMAIL_NULL_PARAMETERS);
+	ASSERT_TEST(emailCreate(NULL, &mail) == EMAIL_NULL_PARAMETERS);
+	ASSERT_TEST(emailCreate(good_mail, NULL) == EMAIL_NULL_PARAMETERS);
+	ASSERT_TEST(emailCreate(bad_mail_1, &mail) == EMAIL_INVALID_PARAMETERS);
+	ASSERT_TEST(emailCreate(bad_mail_2, &mail) == EMAIL_INVALID_PARAMETERS);
+	ASSERT_TEST(emailCreate(good_mail, &mail) == EMAIL_SUCCESS);
+	ASSERT_TEST(mail != NULL);
+	emailDestroy(mail);
+	return true;
+}
+
+/**
+ * Test copy method
+ */
+static bool testEmailCopy() {
+	char* good_mail = "awsome@mail.com";
+	Email mail, copy = NULL;
+	emailCreate(good_mail, &mail);
+	ASSERT_TEST(emailCopy(NULL, NULL) == EMAIL_NULL_PARAMETERS);
+	ASSERT_TEST(emailCopy(mail, NULL) == EMAIL_NULL_PARAMETERS);
+	ASSERT_TEST(emailCopy(NULL, &copy) == EMAIL_NULL_PARAMETERS);
+	ASSERT_TEST(emailCopy(mail, &copy) == EMAIL_SUCCESS);
+	ASSERT_TEST(emailAreEqual(mail, copy));
+	emailDestroy(copy);
+	emailDestroy(mail);
+	return true;
+}
