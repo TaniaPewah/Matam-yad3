75eb256c2271068952cbd09ed843a82863b945c6
alondBind <binder@binder-pc>
2016-05-10 16:12:35 +0300

    s

--- src/main.c
+++ src/main.c
@@ -4,26 +4,8 @@
 #include "aux_macros.h"
 #include "yad3Program.h"
 
-/*int RunEmailTest();
-int RunAgentTest();
-int	RunAgentManagerTest();
-int RunClientTest();
-int RunClientManagerTest();
-int RunClientPurchaseBillTest();
-int RunOfferTest();
-int RunAgentDetailsTest();
-int RunYad3ServiceTest();
-int RunOffersManagerTest();*/
-
 int main(int argc, char *argv[]) {
-	char a0[] = "stam.exe";
-	char a1[] = "-i";
-	char a2[] = "C:\\Users\\binder\\Downloads\\Technion\\test\\in.in";
-	char a3[] = "-o";
-	char a4[] = "C:\\Users\\binder\\Downloads\\Technion\\test\\out.out";
-	char* values[] = { a0, a1, a2, a3, a4};
-	Yad3Program prog = yad3ProgramCreate(values,5);
-	//Yad3Program prog = yad3ProgramCreate(argv, argc);
+	Yad3Program prog = yad3ProgramCreate(argv, argc);
 	if (prog != NULL) {
 		yad3ProgramRun(prog);
 		yad3ProgramDestroy(prog);
